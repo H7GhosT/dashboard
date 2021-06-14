7978fd6c1e15f59bdcacac67a0fdae445b20c2f7
Daniel Suruceanu <daniel.suruceanu@internal.ebs.md>
2021-06-14 14:45:59 +0300

    minor fixes

--- src/components/common/full-view.tsx
+++ src/components/common/full-view.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { addClsToProps } from "./utils";
+import { addClsToProps } from "../utils";
 
 export function FullView(props: React.HTMLAttributes<HTMLDivElement>) {
   return <div {...addClsToProps(props, "full-view")}></div>;
--- src/components/common/icon.tsx
+++ src/components/common/icon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { addClsToProps } from "./utils";
+import { addClsToProps } from "../utils";
 
 export function Icon(props: React.HTMLAttributes<HTMLSpanElement>) {
   return <span {...addClsToProps(props, "material-icons")}></span>;
